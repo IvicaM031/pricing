99072a36243736e6b24cc5a8d5eceb597046d88b
Ivica M <imartinkovic97@gmail.com>
2021-12-19 21:58:08 +0100

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Get the container element
+
 var btnContainer = document.getElementsByClassName("period");
 
 // Get all buttons with class="btn" inside the container
@@ -8,9 +8,9 @@ function yearly()
     document.getElementById("basicprice").innerHTML = "<span>$570</span>/ Y";
     document.getElementById("proprice").innerHTML = "<span>$1100</span>/ Y";
     document.getElementById("busprice").innerHTML = "<span>$1700</span>/ Y";
-    var button = document.getElementsByClassName("btn")[1];
+    let button = document.getElementsByClassName("btn")[1];
     button.classList.add('active');
-    var button = document.getElementsByClassName("btn")[0];
+    button = document.getElementsByClassName("btn")[0];
     button.classList.remove('active');
 }
 function monthly()
@@ -18,9 +18,9 @@ function monthly()
     document.getElementById("basicprice").innerHTML = "<span>$49</span>/month";
     document.getElementById("proprice").innerHTML = "<span>$99</span>/month";
     document.getElementById("busprice").innerHTML = "<span>$149</span>/month";
-    var button = document.getElementsByClassName("btn")[0];
+    let button = document.getElementsByClassName("btn")[0];
     button.classList.add('active');
-    var button = document.getElementsByClassName("btn")[1];
+    button = document.getElementsByClassName("btn")[1];
     button.classList.remove('active');
 }
 
